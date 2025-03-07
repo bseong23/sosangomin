301b65a1ac9993bc093d841cf5537eccf5176dd3
kwonis <oninseoung@naver.com>
2025-03-07 12:47:11 +0900

    :bug:[Fix] 폴더명 수정

---
+++ sosangomin-client/src/components/header/Header.tsx
@@ -0,0 +1,67 @@
+// src/components/Header.tsx
+
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { NavItem, UserInfo } from '@/types/header';
+import Logo from '@/assets/Logo.svg';
+import Profile from '@/assets/profile.svg';
+
+const Header: React.FC = () => {
+  const location = useLocation();
+  const token = localStorage.getItem('token');
+
+  const navItems: NavItem[] = [
+    { name: '서비스 소개', path: '/service' },
+    { name: '데이터 분석', path: '/data-analysis/upload' },
+    { name: '상권분석', path: '/map' },
+    { name: '커뮤니티', path: '/community/notice' },
+  ];
+
+  const userInfo: UserInfo = {
+    name: '박보성'
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-between border-b border-gray-300 h-[73px] font-inter">
+      <div className="pl-[28px]">
+        <Link to="/">
+          <img src={Logo} alt="로고" className="w-[116px] h-[38px] cursor-pointer" />
+        </Link>
+      </div>
+
+      <div className="flex gap-[100px]">
+        {navItems.map((item) => (
+          <Link
+            key={item.path}
+            to={item.path}
+            className={`cursor-pointer hover:text-blue-500 text-gray-600 text-[16px] ${
+              location.pathname === item.path ? 'font-extrabold' : ''
+            }`}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      <div className="flex pr-[40px]">
+        {token ? (
+          <>
+            <img src={Profile} alt="프로필" className="flex h-[41px] w-[41px] rounded-full" />
+            <div className="flex flex-col items-center pl-[12px]">
+              <p className="flex text-gray-600 text-[16px]">환영합니다</p>
+              <p className="flex text-gray-600 text-[16px]">{userInfo.name}님</p>
+            </div>
+          </>
+        ) : (
+          <Link
+            to="/signup"
+            className="flex items-center justify-center bg-[#16125D] text-white px-[25px] py-[12px] rounded-md hover:bg-blue-800 w-[116px] h-[40px]"
+          >
+            로그인
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
