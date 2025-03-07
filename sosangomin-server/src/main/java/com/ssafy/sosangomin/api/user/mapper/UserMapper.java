39d67af2daa54ec9a8395d17ea95299c09ddf7a6
Son-Hunseo <2423aa@naver.com>
2025-03-07 11:20:30 +0900

    :sparkles: [Feat] UserMapper 추가

--- sosangomin-server/src/main/java/com/ssafy/sosangomin/api/user/mapper/UserMapper.java
+++ sosangomin-server/src/main/java/com/ssafy/sosangomin/api/user/mapper/UserMapper.java
@@ -1,4 +1,27 @@
 package com.ssafy.sosangomin.api.user.mapper;
 
-public class UserMapper {
+import com.ssafy.sosangomin.api.user.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM users WHERE id = #{id}")
+    Optional<User> findUserById(@Param("id") Long id);
+
+    @Select("SELECT * FROM users WHERE socail_id = #{socailId}")
+    Optional<User> findUserBySocialId(@Param("socialId") String socialId);
+
+    @Insert("INSERT INTO users (social_id, name, profile_img_url) " +
+             "VALUES (#{socailId}, #{name}, #{profileImgUrl})")
+    void insertKakaoUser(
+            @Param("socailId") String socialId,
+            @Param("name") String name,
+            @Param("profileImgUrl") String profileImgUrl
+    );
 }
