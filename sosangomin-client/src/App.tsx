cef2a1bb0a03bd314593d2d12296364b052bd89a
kwonis <oninseoung@naver.com>
2025-03-07 11:05:13 +0900

    :sparkles: [Feat] URL 작성

--- sosangomin-client/src/App.tsx
+++ sosangomin-client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "@/components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Footer from "@/components/Footer/Footer";
 
 const App = () => {
   return (
@@ -9,7 +9,22 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<p className="text-4xl font-bold text-blue-500 mt-4 h-100%">메인페이지</p>} />
-        {/* 다른 라우트 추가 가능 */}
+        <Route path="/login" />
+        <Route path="/signup" />
+        <Route path="/reset-password" />
+        <Route path="/mypage" />
+        <Route path="/community/notice" />
+        <Route path="/community/notice/post/:postId" />
+        <Route path="/community/news" />
+        <Route path="/community/board" />
+        <Route path="/community/board/write" />
+        <Route path="/community/board/edit" />
+        <Route path="/community/board/post/:postId" />
+        <Route path="/data-analysis/upload" />
+        <Route path="/data-analysis/insight" />
+        <Route path="/data-analysis/review-insight" />
+        <Route path="/data-analysis/advise" />
+        <Route path="/map" />
       </Routes>
       <Footer />
     </Router>
