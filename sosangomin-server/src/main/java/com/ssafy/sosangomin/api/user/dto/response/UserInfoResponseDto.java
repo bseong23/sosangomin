9692ff3dec75affc61e0c8e164d38f8b5fbd2aee
Son-Hunseo <2423aa@naver.com>
2025-03-12 12:44:24 +0900

    :bug:[Fix] 컨벤션에맞게 email -> mail

--- sosangomin-server/src/main/java/com/ssafy/sosangomin/api/user/dto/response/UserInfoResponseDto.java
+++ sosangomin-server/src/main/java/com/ssafy/sosangomin/api/user/dto/response/UserInfoResponseDto.java
@@ -7,7 +7,7 @@ public record UserInfoResponseDto(
         @Schema(description = "유저의 회원가입 타입 (일반로그인, 카카오로그인)")
         UserType userType,
         @Schema(description = "이메일 (카카오 로그인의 경우 null)")
-        String email,
+        String mail,
         @Schema(description = "유저 닉네임")
         String name,
         @Schema(description = "유저 프로필 사진 url, null일 수 있음")
