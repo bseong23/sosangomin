6958d65bcac9ad9af2135001ee23fc29ee8ef022
서지윤 <blahwah1@naver.com>
2025-03-28 10:59:43 +0900

    :bug:[Fix]메인페이지, 위로가기 버튼

--- sosangomin-client/src/pages/MainPage.tsx
+++ sosangomin-client/src/pages/MainPage.tsx
@@ -1,7 +1,6 @@
 // import React from "react";
 import HeroSection from "@/components/main/HeroSection";
 import FeatureSection from "@/components/main/FeatureSection";
-import TopButton from "@/components/common/TopButton";
 import CTASection from "@/components/main/CTASection";
 
 const MainPage = () => {
