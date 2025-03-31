c4cb370fbf048eb1a821ba6f6c977a8622e8a0ef
boseong <qkrqhd23@naver.com>
2025-03-31 12:56:17 +0900

    :bug:[Fix] 유효하지 않은 토큰일 시 ..

--- sosangomin-client/src/api/axios.ts
+++ sosangomin-client/src/api/axios.ts
@@ -1,6 +1,7 @@
 // src/api/axios.ts
-import axios from "axios";
-import { getAccessToken } from "@/features/auth/api/userStorage";
+import axios, { AxiosError } from "axios";
+import { getAccessToken, clearAuthData } from "@/features/auth/api/userStorage";
+import useAuthStore from "@/store/useAuthStore";
 
 // 기본 axios 인스턴스 생성
 const API_URL = import.meta.env.VITE_API_SERVER_URL || "";
@@ -22,6 +23,35 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 - 유효하지 않은 토큰 처리
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    //     // if (error.response?.status === 401) {
+    //     //   // 로컬 스토리지 데이터 삭제
+    //     //   clearAuthData();
+
+    //     //   // Zustand 스토어 직접 접근
+    //     //   useAuthStore.getState().clearUserInfo();
+
+    //     //   // 로그인 페이지로 리다이렉트
+    //     //   window.location.href = "/login";
+    //     // }
+
+    if (error.code === "ERR_NETWORK" || !error.response) {
+      clearAuthData();
+
+      useAuthStore.getState().clearUserInfo();
+
+      window.location.href = "/login";
+
+      console.log("네트워크 에러 발생:", error.message);
+    }
+
     return Promise.reject(error);
   }
 );
