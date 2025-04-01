84e1a12bdf91baaa5cb9e5d90968fc1a0be1abb0
ehgud937 <ehgud937@gmail.com>
2025-04-01 11:18:44 +0900

    :sparkles: [Feat] Store Proxt add

---
+++ sosangomin-server/src/main/java/com/ssafy/sosangomin/api/proxy/store/dto/StoreDetailResponse.java
@@ -0,0 +1,9 @@
+package com.ssafy.sosangomin.api.proxy.store.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
+public record StoreDetailResponse(
+        @Schema(description = "응답 상태", example = "success")
+        String status,
+
+        @Schema(description = "매장 정보")
+        Store store
+) {}
