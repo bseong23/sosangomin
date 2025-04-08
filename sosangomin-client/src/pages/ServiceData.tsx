2693326307c1d7e4dc57fb9cebcdb178aa32fc80
서지윤 <blahwah1@naver.com>
2025-04-08 16:05:34 +0900

    :bug:[Fix]리뷰 및 비교 분석 소개 페이지

--- sosangomin-client/src/pages/ServiceData.tsx
+++ sosangomin-client/src/pages/ServiceData.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import FeatureCard from "@/features/service/FeatureCard";
 import InsightCards from "@/features/service/InsightCards";
 import StepGuide from "@/features/service/StepGuide";
